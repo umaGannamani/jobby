198f3633e7ef77200d38d8d65227327fbf32197a
uma <gannamaniuma@gmail.com>
2026-07-28 15:00:23 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {useEffect, useState} from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  // GET REQUEST
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  const getUsers = async() => {
+    try {
+      const response
+    }
+  }
+
+}
+
+export default App;
